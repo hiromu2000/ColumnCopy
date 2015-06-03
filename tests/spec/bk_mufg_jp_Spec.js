833bc0003c19cce70f04c05d2ae2edabea2a27ed
Hiromu <hiromu2000@hotmail.com>
2015-06-03 21:54:23 +0900

    Test starts from scraping

--- tests/spec/bk_mufg_jp_Spec.js
+++ tests/spec/bk_mufg_jp_Spec.js
@@ -1,12 +1,12 @@
 describe('bk_mufg_jp', function() {
     var trans;
+    var rows;
     var metadata;
     beforeEach(function(done) {
         jasmine.getFixtures().fixturesPath = '../translators';
         var fixture = readFixtures('bk_mufg_jp.js');
         var m = /\/\*([\S\s]*?)\*\//.exec(fixture);
         metadata = JSON.parse(m[1]);
-        var rows = metadata.test.rows;
         var account_name = metadata.account_name;
         var script = metadata.account_name + '.js';
         if (metadata.custom) {
@@ -14,12 +14,20 @@ describe('bk_mufg_jp', function() {
         } else {
             script = '../translators/default.js';
         }
+        jasmine.getFixtures().fixturesPath = './';
+        loadFixtures('test_bk_mufg_jp.html');
+        var $table = $('table:first');
+        var _ColumnCopy = new ColumnCopy();
+        rows = _ColumnCopy.getValuesForTable($table);
         $.getScript(script, function() {
             trans = parse(rows, metadata);
             done();
         });
     });
-    it('Transaction check', function() {
+    it('Scraper check', function() {
+        expect(rows).toEqual(metadata.test.rows);
+    });
+    it('Translator check', function() {
         expect(trans).toEqual(metadata.test.trans);
     });
 });
