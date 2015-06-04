4acd88d31c792add62a35fd4bbefbd00a8b24068
Hiromu <hiromu2000@hotmail.com>
2015-06-04 23:10:48 +0900

    Translator for mufgcard.com

---
+++ translators/mufgcard_com.js
@@ -0,0 +1,14 @@
+/*
+{
+    "target": "^https://www2\\.mufgcard\\.com|test_mufgcard_com\\.html$",
+    "account_name": "mufgcard_com",
+    "custom": false,
+    "date_format": "YYYY/M/D",
+    "column_date": 0,
+    "column_name": 1,
+    "column_memo": 5,
+    "column_deposit": null,
+    "column_withdraw": 4,
+    "amount_separator": "[^-0-9]"
+}
+*/
