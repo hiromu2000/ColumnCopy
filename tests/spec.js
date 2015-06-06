776ac5ba72166bdbad15ed00a18089bef8d53ca5
Hiromu <hiromu2000@hotmail.com>
2015-06-06 19:34:26 +0900

    Test for ColumnCopy

--- tests/spec.js
+++ tests/spec.js
@@ -1,4 +1,26 @@
-describe('Tests', function() {
+describe('Tests for ColumnCopy', function() {
+    function forEach(fixtureName) {
+        var rows;
+        var spec;
+        beforeEach(function() {
+            jasmine.getFixtures().fixturesPath = './';
+            var fixture = readFixtures(fixtureName);
+            setFixtures(fixture);
+            var m = /<!--([\S\s]*?)-->/.exec(fixture);
+            spec = JSON.parse(m[1]);
+            var $table = $('table:first');
+            var _ColumnCopy = new ColumnCopy();
+            _ColumnCopy.options = {hyperlinkMode: 'off'};
+            rows = _ColumnCopy.getValuesForTable($table);
+        });
+        it('Translator check for ' + fixtureName, function() {
+            expect(rows).toEqual(spec.rows);
+        });
+    }
+    forEach('test1.html');
+});
+
+describe('Tests for TransactionTableCopy', function() {
     function forEach(translatorName) {
         var trans;
         var rows;
